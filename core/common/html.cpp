e9acedd5b1e15e9c54d8839e469bd626fd6e04b5
ami <github@amisuke.com>
2026-09-19 12:22:01 +0900

    sprintfの使用をsnprintfに置き換え、バッファオーバーフローを防止

--- core/common/html.cpp
+++ core/common/html.cpp
@@ -181,11 +181,11 @@ void HTML::addHead()
         startTagEnd("meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"");
         if (!refreshURL.isEmpty())
         {
-            sprintf(buf, "meta http-equiv=\"refresh\" content=\"%d;URL=%s\"", refresh, cgi::escape_html(refreshURL).c_str());
+            snprintf(buf, sizeof(buf), "meta http-equiv=\"refresh\" content=\"%d;URL=%s\"", refresh, cgi::escape_html(refreshURL).c_str());
             startTagEnd(buf);
         }else if (refresh)
         {
-            sprintf(buf, "meta http-equiv=\"refresh\" content=\"%d\"", refresh);
+            snprintf(buf, sizeof(buf), "meta http-equiv=\"refresh\" content=\"%d\"", refresh);
             startTagEnd(buf);
         }
     end();
@@ -235,7 +235,7 @@ void HTML::addLink(const char *url, const char *text, bool toblank)
 {
     char buf[1024];
 
-    sprintf(buf, "a href=\"%s\" %s", url, toblank?"target=\"_blank\"":"");
+    snprintf(buf, sizeof(buf), "a href=\"%s\" %s", url, toblank?"target=\"_blank\"":"");
     startNode(buf, text);
     end();
 }
@@ -249,7 +249,7 @@ void HTML::startTag(const char *tag, const char *fmt, ...)
         va_start(ap, fmt);
 
         char tmp[512];
-        vsprintf(tmp, fmt, ap);
+        vsnprintf(tmp, sizeof(tmp), fmt, ap);
         startNode(tag, tmp);
 
         va_end(ap);
@@ -267,7 +267,7 @@ void HTML::startTagEnd(const char *tag, const char *fmt, ...)
         va_start(ap, fmt);
 
         char tmp[512];
-        vsprintf(tmp, fmt, ap);
+        vsnprintf(tmp, sizeof(tmp), fmt, ap);
         startNode(tag, tmp);
 
         va_end(ap);
@@ -284,7 +284,7 @@ void HTML::startSingleTagEnd(const char *fmt, ...)
     va_start(ap, fmt);
 
     char tmp[512];
-    vsprintf(tmp, fmt, ap);
+    vsnprintf(tmp, sizeof(tmp), fmt, ap);
     startNode(tmp);
 
     va_end(ap);
